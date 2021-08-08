0b5a0d115b5639eee81c5805d28049937e7c5b98
Mark Andersen <10101087+MarkAndersen@users.noreply.github.com>
2021-08-08 19:11:50 -0400

    service worker additions

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,20 @@
 console.log('working!')
+
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/styles.css",
+    "/manifest.webmanifest",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+];
+const STATIC_CACHE = "static-cache-v1"
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches
+        .open(STATIC_CACHE)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+        .then(() => self.skipWaiting())
+    );
+});
